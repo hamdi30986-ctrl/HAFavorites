4b6f842221b651ee624dc2320c77331d7788b7f9
Hamdi-v39y <hamdi30986@gmail.com>
2025-12-31 19:06:37 +0300

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -10,14 +10,12 @@ from . import DOMAIN
 
 
 class FavoritesConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Favorites."""
 
     VERSION = 1
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
-        """Handle the initial step."""
         # Only allow a single instance
         await self.async_set_unique_id(DOMAIN)
         self._abort_if_unique_id_configured()
